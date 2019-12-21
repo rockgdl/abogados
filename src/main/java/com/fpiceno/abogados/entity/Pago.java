8a5e0f266199e6ce3fd70cf9349adc78860ced9b
Adan Piceno <metalgdl15@gmail.com>
2019-12-20 20:00:05 -0600

    Test Pago

--- src/main/java/com/fpiceno/abogados/entity/Pago.java
+++ src/main/java/com/fpiceno/abogados/entity/Pago.java
@@ -31,5 +31,75 @@ public class Pago {
     
     @Column (name = "cantidad")
     private int cantidad;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the idFacutura
+     */
+    public int getIdFacutura() {
+        return idFacutura;
+    }
+
+    /**
+     * @param idFacutura the idFacutura to set
+     */
+    public void setIdFacutura(int idFacutura) {
+        this.idFacutura = idFacutura;
+    }
+
+    /**
+     * @return the concepto
+     */
+    public String getConcepto() {
+        return concepto;
+    }
+
+    /**
+     * @param concepto the concepto to set
+     */
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    /**
+     * @return the fechaPago
+     */
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+    /**
+     * @param fechaPago the fechaPago to set
+     */
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
     
 }
---
+++ src/test/java/pagoTest.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Tools.tipoPago;
+import com.fpiceno.abogados.dao.PagoDao;
+import com.fpiceno.abogados.dao.mysql.PagoDaoMysql;
+import com.fpiceno.abogados.entity.Pago;
+import java.util.Date;
+import java.util.Iterator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gnr_a
+ */
+public class pagoTest {
+    private Date date = new Date();
+    
+    public pagoTest() {
+    }
+    
+    @Test
+    public void add(){
+        PagoDao dao = new PagoDaoMysql();
+        
+        Pago pago = new Pago();
+        
+        pago.setConcepto("Pago 1");
+        pago.setIdFacutura(1);
+        pago.setFechaPago(date);
+        pago.setCantidad(3);
+             
+        dao.insert(pago);
+        
+        pago.setConcepto("Pago 2");
+        pago.setIdFacutura(1);
+        pago.setFechaPago(date);
+        pago.setCantidad(3);
+             
+        dao.insert(pago);
+        
+        pago.setConcepto("Pago 3");
+        pago.setIdFacutura(2);
+        pago.setFechaPago(date);
+        pago.setCantidad(3);
+             
+        dao.insert(pago);
+        
+    }
+    
+    @Test
+    public void read(){
+        PagoDao dao = new PagoDaoMysql();
+        
+        Iterator<Pago> i = dao.read().iterator();
+        
+        while(i.hasNext()){
+            System.out.println(i.next().getConcepto());
+        }
+    }
+    
+    @Test
+    public void update(){
+        PagoDao dao = new PagoDaoMysql();
+        
+        Pago pago = new Pago();
+        pago.setId(1);
+        pago.setConcepto("Pago Modificado");
+        pago.setIdFacutura(4);
+        pago.setFechaPago(date);
+        pago.setCantidad(4);
+        
+        
+        dao.update(pago);
+    }
+    
+    @Test
+    public void delete(){
+        PagoDao dao = new PagoDaoMysql();
+        
+        Pago pago = new Pago();
+        pago.setId(2);
+        
+        dao.delete(pago);
+    }
+    
+}
