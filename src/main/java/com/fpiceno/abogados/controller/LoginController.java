de4013c0ad561dfdd4401efe447fbdc8692a990b
fpiceno <fpiceno@LT-FABIAN1.grupourrea.com>
2019-12-27 16:41:51 -0600

    se añadio el logo en el controller y se añadio la imagen

--- src/main/java/com/fpiceno/abogados/controller/LoginController.java
+++ src/main/java/com/fpiceno/abogados/controller/LoginController.java
@@ -3,10 +3,13 @@ package com.fpiceno.abogados.controller;
 import com.fpiceno.abogados.entity.Usuario;
 import com.fpiceno.abogados.dao.mysql.UsuarioDaoMysql;
 import com.fpiceno.abogados.dao.UsuarioDao;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,8 +19,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 public class LoginController implements Initializable {
     
@@ -25,7 +31,10 @@ public class LoginController implements Initializable {
     
     @FXML private TextField txtNickName;
     @FXML private PasswordField txtPassword;
+    @FXML private AnchorPane rootPane;
+    @FXML private ImageView imageView;
     
+    private final Logger log= Logger.getLogger(LoginController.class.getSimpleName());
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -37,27 +46,35 @@ public class LoginController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+      log.info("entrando a inicializar el contenido del login");
+      FileInputStream input;
+//        try {
+            //input = new FileInputStream("/imagenes/logo1.jpg");
+            Image image = new Image("/imagenes/logo1.jpg");
+         //imageView = new ImageView(image);+
+                 imageView.setImage(image);
+//        } catch (FileNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+
     }    
     
     @FXML
     private void Entrar(ActionEvent event) throws IOException{
                 
         UsuarioDao dao = new UsuarioDaoMysql();
-        
+         AnchorPane content = null;
         Usuario usuario = dao.checkUser(txtNickName.getText(), txtPassword.getText());
-        
-        System.out.println(usuario.getRol());
-        
-        Stage stage = new Stage();
-        FXMLLoader load = new FXMLLoader();
-        AnchorPane root = null;
+        log.info("error al entrar a la aplicacion ");
+       log.info(usuario.getRol());
+  
+       
         
         switch(usuario.getRol()){
             case ADMINISTRADOR: 
-                root = (AnchorPane)load.load(getClass().getResource("FXML/Usuarios.fxml").openStream());                  
-                // con esto ya solo se manda los datos que se quiera al constructor del controllador
-                UsuariosController userController = (UsuariosController) load.getController();
+                 AnchorPane pane = FXMLLoader.load(getClass().getResource("/fxml/Caso.fxml"));
+                 rootPane.getChildren().setAll(pane);
                 break;
                 
             case INGRESOS: break;
@@ -65,10 +82,8 @@ public class LoginController implements Initializable {
             case CONSULTA: break;     
         }
         
-        stage.setScene(new Scene(root));
-        stage.show();
+   
         
-        //Para eliminar una escena
-        ((Node)(event.getSource())).getScene().getWindow().hide();
+
     }
 }
