b83d105e09b54e086b56ecca1d9c58d486da2d29
Adan Piceno <metalgdl15@gmail.com>
2019-12-27 14:56:19 -0600

    corregir login

--- src/main/java/com/fpiceno/abogados/controller/LoginController.java
+++ src/main/java/com/fpiceno/abogados/controller/LoginController.java
@@ -3,15 +3,21 @@ package com.fpiceno.abogados.controller;
 import com.fpiceno.abogados.entity.Usuario;
 import com.fpiceno.abogados.dao.mysql.UsuarioDaoMysql;
 import com.fpiceno.abogados.dao.UsuarioDao;
+import java.io.IOException;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
     
@@ -35,7 +41,7 @@ public class LoginController implements Initializable {
     }    
     
     @FXML
-    private void Entrar(ActionEvent event){
+    private void Entrar(ActionEvent event) throws IOException{
                 
         UsuarioDao dao = new UsuarioDaoMysql();
         
@@ -43,12 +49,26 @@ public class LoginController implements Initializable {
         
         System.out.println(usuario.getRol());
         
+        Stage stage = new Stage();
+        FXMLLoader load = new FXMLLoader();
+        AnchorPane root = null;
+        
         switch(usuario.getRol()){
-            case ADMINISTRADOR: break;
+            case ADMINISTRADOR: 
+                root = (AnchorPane)load.load(getClass().getResource("FXML/Usuarios.fxml").openStream());                  
+                // con esto ya solo se manda los datos que se quiera al constructor del controllador
+                UsuariosController userController = (UsuariosController) load.getController();
+                break;
+                
             case INGRESOS: break;
             case EGRESOS: break;
             case CONSULTA: break;     
         }
-
+        
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+        //Para eliminar una escena
+        ((Node)(event.getSource())).getScene().getWindow().hide();
     }
 }
