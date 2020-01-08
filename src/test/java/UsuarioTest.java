e0560dfac4e9810fa8faa6fa16d1895d9081ab0f
fpiceno <fpiceno@LT-FABIAN1.grupourrea.com>
2020-01-08 17:42:44 -0600

    nuevas cosas por hacer y correccion de bugs

--- src/test/java/UsuarioTest.java
+++ src/test/java/UsuarioTest.java
@@ -21,44 +21,44 @@ public class UsuarioTest {
     
     public UsuarioTest() {
     }
-//    
-//    @Test
-//    public void addUsuario()
-//    {
-//        Date date = new Date();
-//        Usuario usuario=new Usuario();
-//        usuario.setNickName("fabian");
-//        usuario.setPassword("123");
-//        usuario.setRol(Roles.ADMINISTRADOR);
-//        usuario.setActivo(true);
-//        usuario.setFechaCreacion(date);
-//        
-//        Usuario usuario2=new Usuario();
-//        usuario2.setNickName("Adan");
-//        usuario2.setPassword("123");
-//        usuario2.setRol(Roles.EGRESOS);
-//        usuario2.setActivo(false);
-//        usuario2.setFechaCreacion(date);
-//        
-//        UsuarioDao dao=new UsuarioDaoMysql();
-//        dao.insert(usuario);
+    
+    @Test
+    public void addUsuario()
+    {
+        Date date = new Date();
+        Usuario usuario=new Usuario();
+        usuario.setNickName("rockgdl");
+        usuario.setPassword("123");
+        usuario.setRol(Roles.ADMINISTRADOR);
+        usuario.setActivo(true);
+        usuario.setFechaCreacion(date);
+        
+        Usuario usuario2=new Usuario();
+        usuario2.setNickName("Adan");
+        usuario2.setPassword("123");
+        usuario2.setRol(Roles.EGRESOS);
+        usuario2.setActivo(false);
+        usuario2.setFechaCreacion(date);
+        
+        UsuarioDao dao=new UsuarioDaoMysql();
+        dao.insert(usuario);
 //        dao.insert(usuario2);
-//    }
-//    
-//    @Test
-//    public void update(){
-//        Date date = new Date();
-//        
-//        Usuario usuario2=new Usuario();
-//        usuario2.setId(2);
-//        usuario2.setNickName("Adan");
-//        usuario2.setPassword("12343");
-//        usuario2.setRol(Roles.INGRESOS);
-//        usuario2.setActivo(false);
-//        usuario2.setFechaCreacion(date);
-//        
-//        UsuarioDao dao=new UsuarioDaoMysql();
-//        dao.update(usuario2);
-//    }
+    }
+    
+    @Test
+    public void update(){
+        Date date = new Date();
+        
+        Usuario usuario2=new Usuario();
+        usuario2.setId(2);
+        usuario2.setNickName("Adan");
+        usuario2.setPassword("12343");
+        usuario2.setRol(Roles.INGRESOS);
+        usuario2.setActivo(false);
+        usuario2.setFechaCreacion(date);
+        
+        UsuarioDao dao=new UsuarioDaoMysql();
+        dao.update(usuario2);
+    }
     
 }
