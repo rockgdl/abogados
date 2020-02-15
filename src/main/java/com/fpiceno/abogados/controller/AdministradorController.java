269f4ad3fd7ff758e2fd87745aa66900064d174a
fpiceno <fpiceno@LT-FABIAN1.grupourrea.com>
2020-02-14 18:02:13 -0600

    se agrego la escena en segunda ventana

--- src/main/java/com/fpiceno/abogados/controller/AdministradorController.java
+++ src/main/java/com/fpiceno/abogados/controller/AdministradorController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -42,6 +43,7 @@ public class AdministradorController implements Initializable {
         try {
             root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+                 stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root1));  
             stage.show();
         } catch (IOException ex) {
@@ -59,6 +61,7 @@ public class AdministradorController implements Initializable {
         try {
             root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+                 stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root1));  
             stage.show();
         } catch (IOException ex) {
@@ -77,6 +80,7 @@ public class AdministradorController implements Initializable {
         try {
             root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+                 stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root1));  
             stage.show();
         } catch (IOException ex) {
@@ -92,6 +96,7 @@ public class AdministradorController implements Initializable {
         try {
             root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root1));  
             stage.show();
         } catch (IOException ex) {
